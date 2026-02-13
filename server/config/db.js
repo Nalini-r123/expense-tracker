7e2d6ae52a48f7195fc6b98c6ee5f0f0b0caaf72
Nalini-r123 <nalinilashrav@gmail.com>
2026-02-13 10:41:16 +0530

    updates DB config for production

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,14 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = pool.promise();
+module.exports = db;
